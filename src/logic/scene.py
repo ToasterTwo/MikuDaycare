c58dcf3cb97707d0e969b00d161589cd30554f7d
Stasiek Toster <stasiek.wypychowski@gmail.com>
2025-05-26 19:56:11 +0200

    26-05-2025: 0.0.4 finished scene json loading feature

--- src/logic/scene.py
+++ src/logic/scene.py
@@ -1,5 +1,3 @@
-from dis import CACHE
-
 from logic.game_components import *
 from enum import Enum
 from logic.logic_resource import *
@@ -14,6 +12,7 @@ class Scene:
             self._objects.append(obj)
     
     def update(self, delta_time: float):
+        Hitbox.recalculate_collisions()
         for obj in self._objects:
             obj.update(delta_time)
     
@@ -21,7 +20,7 @@ class Scene:
         for obj in self._objects:
             obj.on_event(event)
     
-    def get_resources(self):
+    def get_resources(self) -> tuple[LogicResource, ...]:
         resources = []
         for obj in self._objects:
             for comp in obj.get_components():
@@ -30,18 +29,95 @@ class Scene:
                 
                 elif type(comp) == Rectangle:
                     resources.append(LogicResource.of_shape(comp))
+                
+                elif type(comp) == Hitbox:
+                    global_bounds = comp.get_global_bounds()
+                    gx = (global_bounds[1]+global_bounds[3])/2
+                    gy = (global_bounds[0]+global_bounds[2])/2
+                    resources.append(LogicResource(
+                        ResourceType.SHAPE, 
+                        dimensions = [global_bounds[3]-global_bounds[1], global_bounds[2]-global_bounds[0]],
+                        scale = [1, 1],
+                        color = [0x00, 0xff, 0x00, 0xf0],
+                        global_position = [gx, gy],
+                        layer = 11
+                        ))
 
         return tuple(resources)
 
 
-class SceneLoader:
+def from_json(filepath:str):
+    parsed = None
+    with open(filepath, mode="r",encoding="UTF-8") as file:
+        parsed = json.loads(file.read())
 
-    @staticmethod
-    def from_json(filepath:str):
-        parsed = None
-        with open(filepath, mode="r",encoding="UTF-8") as file:
-            parsed = json.loads(file.read())
+    object_dict: dict[str, Any] = {}
+    objects = []
+    for o in parsed:
+        parent_name : str = parsed[o]["Parent"]
+        parent: GameObject | None = None
+        if parent_name in object_dict:
+            parent = object_dict[parent_name]
+        
+        object_dict[o] = GameObject(parent = parent)
+        objects.append(object_dict[o])
+    
+    incomplete = []
+    for o in parsed:
+        for comp in parsed[o]["Components"]:
+            comp["Name"] = o+"."+comp["Name"] # type: ignore
+            object_dict[comp["Name"]] = None
 
-        object_dict = {}
-        for o in parsed:
-            print(o, parsed[o])
+    for o in parsed:
+        for comp in parsed[o]["Components"]:
+            args = comp["args"][:]
+            for i in range(len(args)):
+                if type(args[i]) == list:
+                    continue
+                if args[i] in object_dict:
+                    requested = object_dict[args[i]]
+                    if requested is None:
+                        incomplete.append(comp)
+                    args[i] = requested
+            
+            kwargs = comp["kwargs"].copy()
+            for a in kwargs:
+                if type(kwargs[a]) == list:
+                    continue
+
+                if kwargs[a] in object_dict:
+                    requested = object_dict[kwargs[a]]
+                    if requested is None:
+                        incomplete.append(comp)
+                    kwargs[a] = requested
+            
+            comp_obj = Component.from_name(comp["Type"], object_dict[o], *args, **kwargs)
+            object_dict[comp["Name"]] = comp_obj
+            object_dict[o].add_components(comp_obj)
+            
+    
+    for revisited in incomplete:
+        args = revisited["args"]
+        for i in range(len(args)):
+            if args[i] in object_dict:
+                if type(args[i]) == list:
+                    continue
+                args[i] = object_dict[args[i]]
+        
+        kwargs = revisited["kwargs"]
+        for a in kwargs:
+            if type(kwargs[a]) == list:
+                    continue
+            if kwargs[a] in object_dict:
+                kwargs[a] = object_dict[kwargs[a]]
+
+        
+        revisited_obj = object_dict[revisited["Name"]]
+        revisited_obj.__init__(revisited_obj._parent, *args, **kwargs)
+    
+    scene = Scene()
+    scene.add_objects(*objects)
+
+    return scene
+
+
--- src/test_scene.py
+++ src/test_scene.py
@@ -1,39 +1,7 @@
-import logic
-import logic.creature
-import logic.progress_bar
 import logic.scene as scn
-import logic.game_components as gcomp
-
+import logic.progress_bar
+import logic.creature
 
 def make()->scn.Scene:
-    bar_shape = gcomp.Rectangle(dimensions=(100, 10), color = (0xff, 0, 0), position=[0., 0.])
-    bar_script = logic.progress_bar.ProgressBar(None, 10, 10, bar_shape, logic.progress_bar.BarMode.HORIZONTAL, logic.progress_bar.BarAligngment.TOPLEFT)
-    bar_transform = gcomp.Transform(None, [0., 0.], 0)
-    happy_bar = gcomp.GameObject(bar_shape, bar_script, bar_transform)
 
-    creature_brain = logic.creature.CreatureBehaviour(None, happy_bar)
-    miku_image = logic.creature.body_image
-    miku_transform = gcomp.Transform(None, [200.,200.], 0)
-    hair = logic.creature.hair_sprite
-    eyes = logic.creature.eyes_sprite
-    mouth = logic.creature.mouth_sprite
-    left_arm = logic.creature.left_arm_sprite
-    right_arm = logic.creature.right_arm_sprite
-
-
-    miku = gcomp.GameObject(creature_brain,
-                            miku_image, 
-                            miku_transform, 
-                            hair, 
-                            eyes, 
-                            mouth,
-                            left_arm,
-                            right_arm)
-
-    main_scene = scn.Scene()
-    main_scene.add_objects(happy_bar, miku, hair, eyes, mouth, left_arm, right_arm)
-
-    return main_scene
-
-if __name__=="__main__":
-    scene =scn.SceneLoader.from_json("resources/scenes/sample_scene.json")
+    return scn.from_json("resources/scenes/sample.json")
